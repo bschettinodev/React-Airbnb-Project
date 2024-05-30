7bf63694663c9544bd23d2ae989554b11bed49f8
bschettinodev <biel.schettino@gmail.com>
2024-05-30 03:48:40 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+          <Navbar />
+        </div>
+    )
+}
